fb01a40e5f5eb8fa2766bda80ea188fa190048c4
Jonathan Raes <J.M.Raes@student.tudelft.nl>
2013-11-30 22:52:14 +0100

    Test maakt nu gebruik van setup and teardown

--- oop-project/src/OOP/CellTest.java
+++ oop-project/src/OOP/CellTest.java
@@ -1,67 +1,82 @@
 package OOP;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CellTest {
-
+	Cell cell1;
+	Cell cell2;
+	Cell cell3;
+	Cell cell4;
+	Cell cell5;
+	@Before
+	public void setUp(){
+		cell1 = new Cell(1,1,"Een");
+		cell2 = new Cell(1,1,"Een");
+		cell3 = new Cell(2,2,"Twee");
+		cell5 = new Cell(5,6,"Vijf");
+	}
+	
 	@Test
 	public void testCell() {
-		Cell cell1 = new Cell(1,1,"Een");
-		Cell cell2 = new Cell(1,1,"Een");
 		assertNotNull(cell1);
 		assertEquals(cell1,cell2);
+		assertNotEquals(cell2, cell3);
+		assertNotEquals(cell2, cell4);
+	
 	}
 
 	@Test
 	public void testGetRow() {
-		Cell cell1 = new Cell(1,1,"Een");
-		Cell cell2 = new Cell(5,1,"Twee");
 		assertEquals(cell1.getRow(), 1);
-		assertEquals(cell2.getRow(), 5);
+		assertEquals(cell3.getRow(), 2);
 		assertNotEquals(cell1.getRow(), 5);
 	}
 
 	@Test
 	public void testGetCol() {
-		Cell cell1 = new Cell(1,1,"Een");
-		Cell cell2 = new Cell(5,6,"Twee");
 		assertEquals(cell1.getCol(), 1);
-		assertEquals(cell2.getCol(), 6);
+		assertEquals(cell5.getCol(), 6);
 		assertNotEquals(cell1.getRow(), 6);	}
 
 	@Test
 	public void testGetContent() {
-		Cell cell1 = new Cell(1,1,"Een");
-		Cell cell2 = new Cell(5,6,"Twee");
 		assertEquals(cell1.getContent(), "Een");
-		assertNotEquals(cell2.getContent(), "Een");
+		assertNotEquals(cell3.getContent(), "Een");
 	}
 
 	@Test
 	public void testSetContent() {
-		Cell cell1 = new Cell(1,1,"Een");
 		cell1.setContent("Twee");
 		assertEquals(cell1.getContent(), "Twee");
 	}
 
 	@Test
 	public void testSetRow() {
-		Cell cell1 = new Cell(1,1,"Een");
 		cell1.setRow(5);
 		assertEquals(cell1.getRow(), 5);
 	}
 
 	@Test
 	public void testSetCol() {
-		Cell cell1 = new Cell(1,1,"Een");
 		cell1.setCol(5);
 		assertEquals(cell1.getCol(), 5);	}
 
 	@Test
 	public void testToString() {
-		Cell cell1 = new Cell(1,1,"Een");
 		assertEquals(cell1.toString(), "Rij: 1\nKolom: 1\nContents: Een");
 		
+	}
+	
+	@After
+	public void tearDown(){
+		cell1 = null;
+		cell2 = null;
+		cell3 = null;
+		cell5 = null;
 	}
 }
