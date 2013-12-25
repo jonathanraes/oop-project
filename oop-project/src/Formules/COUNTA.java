0e6b4e904f5fdc5e90883eb9e7a7b8209176e715
Keraito <dragoon_v2_chak_shun@hotmail.com>
2013-12-25 14:21:03 +0100

    Javadoc commentaar geschreven voor COUNTA en COUNTIF.

--- oop-project/src/Formules/COUNTA.java
+++ oop-project/src/Formules/COUNTA.java
@@ -1,8 +1,17 @@
 package Formules;
+/**
+ * COUNTA klasse. Hierin wordt de formule COUNTA uitgevoerd. COUNTA telt het aantal cellen in de opgegeven selectie, die niet leeg zijn
+ * Alle binnenkomende waardes in de String[] a wordt vergelijken met "" en null, om te kijken of het niet leeg is.
+ * Indien het niet leeg is, en dus ongelijk aan "" en null, dan wordt het meegerekent met de counter.
+ */
 
 
 public class COUNTA extends Formule {
-
+/**
+ * executable, hierin wordt de inhoudelijk werking van COUNTA uitgevoerd
+ * @param String[] waardes: De String array met de waardes van de geselecteerde cellen.
+ * @return String met het aantal niet lege cellen in de selectie aan cellen.
+ */
 	public String executable(String[] a) {
 		// Return waarde wordt geinitializeerd op 0
 		int countnummer = 0;
--- oop-project/src/Formules/COUNTIF.java
+++ oop-project/src/Formules/COUNTIF.java
@@ -1,8 +1,16 @@
 package Formules;
-
-
+/**
+ * COUNTIF klasse. Hierin wordt de formule COUNTIF uitgevoerd. COUNTIF telt het aantal cellen in de
+ * geselecteerde reeks aan cellen die voldoen aan een opgegeven criteria door de gebruiker. Dit wordt gedaan
+ * in de vorm van een expressie. Bijv: "32", ">32", "apples", etc.
+ * 
+**/
 public class COUNTIF extends Formule{
-
+/**
+ * executable, waar de inhoudelijk werking wordt uitgevoerd
+ * @param String[] waardes: De String array met de waardes van de geselecteerde cellen.
+ * @return String met het aantal cellen die voldoen aan de gespecificeerde criteria.
+ */
 	public String executable(String[] a) {
 		
 		return null;
