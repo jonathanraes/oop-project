7c7fd67364ed9155389796d9f4eb3ff96d92b52d
Keraito <dragoon_v2_chak_shun@hotmail.com>
2014-01-04 13:21:57 +0100

    Commentaar en javadoc aan MAX toegevoegd.

--- oop-project/src/Formules/MAX.java
+++ oop-project/src/Formules/MAX.java
@@ -1,25 +1,44 @@
 package Formules;
 import java.util.*;
 
-
+/**
+ * MAX klasse. Hier wordt van de geselecteerde waardes het grootste getal genomen en gereturnt.
+ */
 public class MAX extends Formule {
-
+	/**
+	 * Hier wordt gecontroleerd welke binnenkomende waardes getallen zijn en van de getallen wordt het grootste getal genomen.
+	 * @param String[] waardes: String array met de waardes van de cellen
+	 * @return String: Hierin wordt het grootste getal teruggegeven.
+	 */
 	public String executable(String[] a) {
 		ArrayList<Double> waardes = new ArrayList<Double>();
+		/*  
+		 * 	In deze for-loop wordt gekeken of elke waarde in de binnenkomende StringLijst een getal is. 
+		 *	Als het een getal is, wordt het toegevoegd aan de waardes ArrayList<Double>. Als het niet een getal is,
+		 *  wordt er niets mee gedaan.
+		**/
 		for(int i=0; i<a.length; i++){
 			try{
 				double parse = Double.parseDouble(a[i]);
 				waardes.add(parse);
 			}catch(NumberFormatException e){
-				waardes.add(0.0);
+				//waardes.add(0.0); <- weggehaald en gewoon niets alten gebeuren, omdat indien er alleen negatieve getallen zijn, de formule 0.0 returnt. *
 			}	
 		}
-		double max = waardes.get(0);
-		for(int i = 1; i < waardes.size(); i++){
-			if(waardes.get(i) > max)
-				max = waardes.get(i);
+		// In plaats daarvan wordt er hier gekeken of de ArrayList leeg is, zo ja, dan wordt 0 gereturnt.
+		if(waardes.isEmpty()){
+			return "" + 0.0 + "";
+		}else{
+			/* Hier wordt per waarde uit de ArrayList gecontroleerd of het groter is dan het opgegeven getal max. Zo ja, dan wordt dat getal de max.
+			 * Op het einde houd je dan het grootste getal over uit de reeks.
+			 */
+			double max = waardes.get(0);
+			for(int i = 1; i < waardes.size(); i++){
+				if(waardes.get(i) > max)
+					max = waardes.get(i);
+			}
+			return "" + max + "";
 		}
-		return "" + max + "";
 	}
 	
 }
