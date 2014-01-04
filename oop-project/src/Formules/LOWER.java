8d099650c455aaf64542cd530460cd319068fb2d
Keraito <dragoon_v2_chak_shun@hotmail.com>
2014-01-04 14:34:33 +0100

    Commentaar bij LOWER formule.

--- oop-project/src/Formules/LOWER.java
+++ oop-project/src/Formules/LOWER.java
@@ -1,9 +1,17 @@
 package Formules;
 
-
+/**
+ * LOWER klasse. Hier wordt de binnenkomende String geconvteerd naar alleen maar kleine letters.
+ */
 public class LOWER extends Formule {
-
+	/**
+	 * Hier wordt de eerste String in de StringLijst a genomen en geconverteerd naar alleen maar kleine letters. Hierbij maken we gebruik van toLowerCase()
+	 * in de klasse String.
+	 * @param String[] waardes: String array met de waardes van de cellen
+	 * @return String: Hierin wordt de String teruggegeven, maar dan alleen in kleine letters.
+	 */
 	public String executable(String[] a) {
+		// Voor het converteren naar kleine letters maken we gebruik van de methode toLowerCase() in de klasse String. Deze returnen we ook direct.
 		return a[0].toLowerCase();
 	}
 
