9de803e04c42efff0edeb06d63db64d7216097ee
Keraito <dragoon_v2_chak_shun@hotmail.com>
2014-01-05 17:03:16 +0100

    COUNTIF en IF afgeprogrammeerd. Testen zal ik zo ook schrijven voor mijn formules. Wat nog mist is een formule die uit een gespecificeerde cel, stel de String "B4", de inhoud uit die cel haalt. Als ik die heb ben ik helemaal klaar. Javadoc en normale commentaar is aan IF COUNTIF toegevoegd, die van IF zal binnenkort ook komen.

--- oop-project/src/Formules/COUNTIF.java
+++ oop-project/src/Formules/COUNTIF.java
@@ -12,11 +12,19 @@ public class COUNTIF extends Formule{
  * @return String met het aantal cellen die voldoen aan de gespecificeerde criteria.
  */
 	public String executable(String[] a) {
-			int countnumber = 0;
+		// Een counter wordt geinitializeerd.	
+		int countnumber = 0;
 			// a[0] is expressie die de volgende structuur heeft: operator(=/>/</<=/>=/<>) en dan cell/getal of String.
 			String expressie = a[0];
+			
+			// De verabelen waarde en operator worden hier alvast geinitizializeerd.
 			String waarde;
 			String operator;
+			
+			/* Hier wordt eerste gekeken naar of de logische expressie een operator heeft. Dit zijn <>, <=, >=, <, > en =.
+			 * Als deze aanwezig zijn wordt afhankelijk van welke operator het is de substring methode opgeroepen om het deel uit de String te halen
+			 * wat geen operator is: de waarde. Vervolgens wordt de operator die gevonden is toegewezen aan operator zodat we deze later kunnen gebruiken.
+			 */
 			if(expressie.contains("<>")){
 				waarde = expressie.substring(2, expressie.length());
 				operator = "<>";
@@ -40,21 +48,32 @@ public class COUNTIF extends Formule{
 			}else if(expressie.contains("=")){
 				waarde = expressie.substring(1, expressie.length());
 				operator = "=";
+				
+			// Indien er geen operator is gevonden, is het dus ook geen logische expressie. De formule returnt dan een error-booschap.
 			}else
 				return "NO VALID LOGICAL EXPRESSION";
 			
 			try{
 				// Eerst wordt gekeken of de variabele waarde (de waarde uit de logische expressie) een getal is.
 				double waardegetal = Double.parseDouble(waarde);
+				/* Vervolgens wordt er gekeken naar welke operator er aanwezig was in de logische expressie. Afhankelijk daarvan wordt er een for-loop
+				 * opgeroepen die de goede operator gebruikt in java om de binnenkomende waardes te vergelijken met de waarde die in de logische expressie staat.
+				 * Indien die waarde een getal is, lukt de parse hierboven en bereiken we dit gedeelte. 
+				 */
 				if(operator.equals("<>")){
 					for(int i=1; i<a.length;i++){
+						/*	Elke waarde uit de binnenkomende selectie wordt geprobeerd te parsen als een getal en vervolgens vergeleken met het getal
+						 * waardegetal afhankelijk van de operator die in het vorige deel is gevonden. Indien de parse niet lukt wordt de onstaande
+						 * NumberFormatException opgevangen. In het catch-blok wordt niets gedaan omdat we alleen iets willen doen met getallen.
+						 * Dit wordt herhaalt voor elke waarde in de binnenkomende selectie.
+						 */
 						try{
 							if(Double.parseDouble(a[i]) != waardegetal)
 								countnumber++;
 						}catch(NumberFormatException NFE2){
 						}
 					}
-					
+				// Zelfde gedeelte als hierboven, maar dan voor operator <=
 				}else if(operator.equals("<=")){
 					for(int i=1; i<a.length;i++){
 						try{
@@ -63,7 +82,7 @@ public class COUNTIF extends Formule{
 						}catch(NumberFormatException NFE2){
 						}
 					}
-					
+				// Zelfde gedeelte als hierboven, maar dan voor operator >=
 				}else if(operator.equals(">=")){
 					for(int i=1; i<a.length;i++){
 						try{
@@ -72,7 +91,7 @@ public class COUNTIF extends Formule{
 						}catch(NumberFormatException NFE2){
 						}
 					}
-					
+				// Zelfde gedeelte als hierboven, maar dan voor operator >
 				}else if(operator.equals(">")){
 					for(int i=1; i<a.length;i++){
 						try{
@@ -81,7 +100,7 @@ public class COUNTIF extends Formule{
 						}catch(NumberFormatException NFE2){
 						}
 					}
-					
+				// Zelfde gedeelte als hierboven, maar dan voor operator <	
 				}else if(operator.equals("<")){
 					for(int i=1; i<a.length;i++){
 						try{
@@ -90,7 +109,7 @@ public class COUNTIF extends Formule{
 						}catch(NumberFormatException NFE2){
 						}
 					}
-					
+				// Zelfde gedeelte als hierboven, maar dan voor operator =
 				}else if(operator.equals("=")){
 					for(int i=1; i<a.length;i++){
 						try{
@@ -100,14 +119,92 @@ public class COUNTIF extends Formule{
 						}
 					}
 				}
-				
+			
+			/*	Indien de parse in het bovenstaande try-block niet lukt, wat betekent dat het geen getal is, wordt de exceptie die opgegooid wordt
+			 * hier opgevangen. Omdat het geen getal is, moet de waarde dus of een cel of een String zijn. 
+			 */
 			}catch(NumberFormatException NFE){
+				/*	Eerst kijken we naar of het een cel is. Hiervoor gebruiken we de methode matches en kijken we door de regex [a-zA-Z]+[1-99]
+				 * of de String bestaat uit een of meerdere letters (kleine of grote letters) met daarna een getal tussen 1 en 99. Deze structuur
+				 * moet gelden over de hele String. Als het een cel is, wordt de inhoud van de cel
+				 * 
+				 */
 				if(waarde.matches("[a-zA-Z]+[1-99]")){
-					String cel = null; // = getcel(waarde) ofzo. <- Waarde ophalen uit cel.
-					for(int i=1;i<a.length;i++){
-						if(a[i].equalsIgnoreCase(cel))
-							countnumber++;
+					String cel = ; // = getcel(waarde) ofzo. <- Waarde ophalen uit cel.
+					if(cel.matches("[0-9]+")){
+						double celgetal = Double.parseDouble(cel);
+						if(operator.equals("<>")){
+							for(int i=1; i<a.length;i++){
+								/*	Elke waarde uit de binnenkomende selectie wordt geprobeerd te parsen als een getal en vervolgens vergeleken met het getal
+								 * celgetal afhankelijk van de operator die in het vorige deel is gevonden. Indien de parse niet lukt wordt de onstaande
+								 * NumberFormatException opgevangen. In het catch-blok wordt niets gedaan omdat we alleen iets willen doen met getallen.
+								 * Dit wordt herhaalt voor elke waarde in de binnenkomende selectie.
+								 */
+								try{
+									if(Double.parseDouble(a[i]) != celgetal)
+										countnumber++;
+								}catch(NumberFormatException NFE2){
+								}
+							}
+						// Zelfde gedeelte als hierboven, maar dan voor operator <=
+						}else if(operator.equals("<=")){
+							for(int i=1; i<a.length;i++){
+								try{
+									if(Double.parseDouble(a[i]) <= celgetal)
+										countnumber++;
+								}catch(NumberFormatException NFE2){
+								}
+							}
+						// Zelfde gedeelte als hierboven, maar dan voor operator >=
+						}else if(operator.equals(">=")){
+							for(int i=1; i<a.length;i++){
+								try{
+									if(Double.parseDouble(a[i]) >= celgetal)
+										countnumber++;
+								}catch(NumberFormatException NFE2){
+								}
+							}
+						// Zelfde gedeelte als hierboven, maar dan voor operator >
+						}else if(operator.equals(">")){
+							for(int i=1; i<a.length;i++){
+								try{
+									if(Double.parseDouble(a[i]) > celgetal)
+										countnumber++;
+								}catch(NumberFormatException NFE2){
+								}
+							}
+						// Zelfde gedeelte als hierboven, maar dan voor operator <	
+						}else if(operator.equals("<")){
+							for(int i=1; i<a.length;i++){
+								try{
+									if(Double.parseDouble(a[i]) < celgetal)
+										countnumber++;
+								}catch(NumberFormatException NFE2){
+								}
+							}
+						// Zelfde gedeelte als hierboven, maar dan voor operator =
+						}else if(operator.equals("=")){
+							for(int i=1; i<a.length;i++){
+								try{
+									if(Double.parseDouble(a[i]) == celgetal)
+										countnumber++;
+								}catch(NumberFormatException NFE2){
+								}
+							}
+						}
+					/*	Als de inhoud van de cel geen getal is, is het een String en moet het gewoon als een String, zonder te letten op hoofd-
+					 * en kleine letters, vergeleken worden met alle waardes uit de binnenkomende selectie.
+					 * 
+					 */
+					}else{
+						for(int i=1;i<a.length;i++){
+							if(a[i].equalsIgnoreCase(cel))
+								countnumber++;
+						}
 					}
+				/*	Als de waarde uit de logische expressie geen getal en geen cel is, moet het dus een String zijn. Hier vergelijken we zonder te letten op 
+				 * hoofdletters en kleine letters of de inhoud van de String en de binnenkomende String hetzelfde zijn. Zo ja, dan wordt de counter verhoogd.
+				 */
 				}else{
 					for(int i=1; i<a.length; i++){
 						if(a[i].equalsIgnoreCase(waarde))
@@ -115,7 +212,7 @@ public class COUNTIF extends Formule{
 					}
 				}
 			}
+			// Als laatste wordt dan de counter gereturnt als een String tussen aanhalingstekens.
 			return "" + countnumber + "";
-	}
-
+		}
 }
