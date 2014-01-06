50b0a32adbf94a3403c51411e06443e390cd0472
Keraito <dragoon_v2_chak_shun@hotmail.com>
2014-01-06 20:44:36 +0100

    Testfile om alle tests in 1x te runnen om makkelijk coverage te bekijken.

---
+++ oop-project/src/Formules/AllTests.java
@@ -0,0 +1,16 @@
+package Formules;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AVERAGETest.class, COUNTATest.class, COUNTEST.class,
+		COUNTIFTest.class, IFTest.class, INTTest.class, ISEVENTest.class,
+		ISLOGICALTest.class, ISNUMBERTest.class, LOWERTest.class,
+		MAXTest.class, MEDIANTest.class, MINTest.class, MODTest.class,
+		POWERTest.class, PRODUCTTest.class, PROPERTest.class, SIGNTest.class,
+		SQRTTest.class, SUMTest.class })
+public class AllTests {
+
+}
