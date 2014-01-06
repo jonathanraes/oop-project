36f99532cad42b81a375da0e0773e8c23b1d0b45
Keraito <dragoon_v2_chak_shun@hotmail.com>
2014-01-06 14:42:20 +0100

    javadoc

---
+++ oop-project/src/Formules/COUNTIFTest.java
@@ -0,0 +1,14 @@
+package Formules;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class COUNTIFTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
