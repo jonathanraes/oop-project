181ce9185a5fd5f13def74c4895e732af195dd55
Keraito <dragoon_v2_chak_shun@hotmail.com>
2014-01-23 16:11:14 +0100

    OR volledig getest. Formules zijn nu 100% branc covered.

--- oop-project/src/Formules/ORTest.java
+++ oop-project/src/Formules/ORTest.java
@@ -18,7 +18,7 @@ public class ORTest {
 	public void test2() {
 		String[] test = {"A1=A4","2","1","A3<A1","2","1","A2=2","2",""};
 		OR or = new OR();
-		System.out.println(or.executable(test));
+		
 		assertEquals(or.executable(test),"TRUE");
 	}
 	
@@ -26,7 +26,7 @@ public class ORTest {
 	public void test3() {
 		String[] test = {"A4>6","10","","3<1","","","2<2","",""};
 		OR or = new OR();
-		System.out.println(or.executable(test));
+		
 		assertEquals(or.executable(test),"TRUE");
 	}
 	
@@ -35,7 +35,7 @@ public class ORTest {
 	public void test4() {
 		String[] test = {"hello>1","","","A1>1","4","","4>5","",""};
 		OR or = new OR();
-		System.out.println(or.executable(test));
+		
 		assertEquals(or.executable(test),"TRUE");
 	}
 	
@@ -43,23 +43,23 @@ public class ORTest {
 	public void test6() {
 		String[] test = {"A4>6","HOI","","3>1","","","2=2","",""};
 		OR or = new OR();
-		System.out.println(or.executable(test));
+		
 		assertEquals(or.executable(test),"TRUE");
 	}
 	
 	@Test
 	public void test7() {
-		String[] test = {"A11>A2","12","2","3<1","","","5<2","",""};
+		String[] test = {"a1>A2","12","2","3<A2","","3","5<2","",""};
 		OR or = new OR();
-		System.out.println(or.executable(test));
+		
 		assertEquals(or.executable(test),"TRUE");
 	}
 	
 	@Test
 	public void test8() {
-		String[] test = {"A4>A6","1","1","3<1","","","A2=A1","2","2"};
+		String[] test = {"A4<>A6","1","1","3<1","","","A2=A1","2","2"};
 		OR or = new OR();
-		System.out.println(or.executable(test));
+		
 		assertEquals(or.executable(test),"TRUE");
 	}
 	
@@ -67,7 +67,7 @@ public class ORTest {
 	public void test9() {
 		String[] test = {"4>=6","","","3<=1","","","2>=1","",""};
 		OR or = new OR();
-		System.out.println(or.executable(test));
+		
 		assertEquals(or.executable(test),"TRUE");
 	}
 	
@@ -75,7 +75,7 @@ public class ORTest {
 	public void test10() {
 		String[] test = {"4=6","","","3=1","","","2=1","",""};
 		OR or = new OR();
-		System.out.println(or.executable(test));
+		
 		assertEquals(or.executable(test),"FALSE");
 	}
 	
@@ -83,9 +83,49 @@ public class ORTest {
 	public void test11() {
 		String[] test = {"A4>=G6","8","12","A3=V1","1","3","A2<=1","4",""};
 		OR or = new OR();
-		System.out.println(or.executable(test));
+		
 		assertEquals(or.executable(test),"FALSE");
 	}
-	
+	@Test
+	public void test12() {
+		String[] test = {"A4G6","8","12","A3=V1","1","3","A2<=1","4",""};
+		OR or = new OR();
+		
+		assertEquals(or.executable(test),"FALSE");
+	}
+	@Test
+	public void test13() {
+		String[] test = {"A4<>G6","8","12","A3=V1","1","3","A2<=1","4",""};
+		OR or = new OR();
+		
+		assertEquals(or.executable(test),"TRUE");
+	}
+	@Test
+	public void test14() {
+		String[] test = {"A4<=G6","8","12","A3=V1","1","3","A2<=1","4",""};
+		OR or = new OR();
+		
+		assertEquals(or.executable(test),"TRUE");
+	}
+	@Test
+	public void test15() {
+		String[] test = {"A4<G6","8","12","A3=V1","1","3","A2<=1","4",""};
+		OR or = new OR();
+		assertEquals(or.executable(test),"TRUE");
+	}
+	@Test
+	public void test16() {
+		String[] test = {"1<>G6","","12","A3=V1","1","3","A2<=1","4",""};
+		OR or = new OR();
+		
+		assertEquals(or.executable(test),"TRUE");
+	}
+	@Test
+	public void test17() {
+		String[] test = {"A4>G6","8","12","A3=V1","1","3","1<A3","","s"};
+		OR or = new OR();
+		
+		assertEquals(or.executable(test),"FALSE");
+	}
 	
 }
